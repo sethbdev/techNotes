e4b3f991741389b2bdf807c1917c97b684493779
SethBrendle <seth.bbrendle@gmail.com>
2023-05-05 01:15:04 -0400

    CORS secured with allowedOrigins.js and corsOptions.js in the config folder. imported and used in server.js

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    //First is for local development
+    'http://localhost:3000',
+    //Second is for production
+    'https://www.example.com',
+    'https://example.com'
+];
+
+module.exports = allowedOrigins;
